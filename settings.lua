6de180f71b61cdd4549ed68ab4a272ccc05373d0
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2023-09-12 15:26:27 -0700

    Create settings.lua

---
+++ settings.lua
@@ -0,0 +1,8 @@
+
+local currency_setting = {
+    type = "string-setting",
+    name = "gamble-currency",
+    setting_type = "runtime-global",
+    default_value = "coin",
+}
+data:extend{currency_setting}
